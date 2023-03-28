d9bbad852af55be19dc472aa2978b615a50d02f9
grace_aloo <graceaaloo@gmail.com>
2023-03-28 15:10:54 +0300

    added to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,54 @@
 class ApplicationController < ActionController::API
+    include ActionController::Cookies 
+    
+    #hash data into web token
+    def encode(uid,email)
+        payload ={
+            data: {
+                uid: uid,
+                email: email
+            },
+            exp: Time.now.to_i + (24 * 3600)
+        }
+        begin
+        JWT.encode(payload, ENV['task train key'], 'HS256')
+        rescue JWT::EncodeError => e
+            render json:{info:"something went wrong"}, status: 400
+
+        end
+    end
+
+    #unhash token
+    def decode(token)
+        begin
+        JWT.decode(token, ENV['task_train_key'],true,{algorithm:'HS256'})
+        rescue JWT::DecodeError => e
+            render json:{info:"your request is not authorized"},status: :unauthorized
+        end
+    end
+
+    #get and save user_id
+    def save_user_id(token)
+        @uid = decode(token)[0]["data"]["uid"].to_i
+    end
+
+    # get logged in user
+    def user
+        User.find(@uid) 
+    end
+
+    def verify_auth
+        auth_headers = request.headers['Authorization']
+        if !auth_headers
+            render json:{info:"Your request is not authorized"}, status: :unauthorized
+        else
+            token = auth_headers.split(' ')[1]
+            save_user_id(token)
+        end
+    end
+
+    #delete token 
+    def remove_user 
+    end
+
 end
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,10 @@ module Phase4Backend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.middleware.use ActionDispatch::Cookies 
+    config.middleware.use ActionDispatch::Session::CookieStore
+    config.action_dispatch.cookies_same_site_protection = :strict 
+
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -18,5 +22,6 @@ module Phase4Backend
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.api_only = true
   end
 end
