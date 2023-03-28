8b140e2f99e9245d2939c5452504790e4e45711b
grace_aloo <graceaaloo@gmail.com>
2023-03-28 14:05:26 +0300

    show and index actions for movie created

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,21 @@
 class MoviesController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    def index 
+        movies = Movie.all 
+        render json: movies 
+    end
+
+    def show 
+        movie = find_movie
+        render json: movie
+    end
+
+    private 
+    def find_movie 
+        Movie.find(params[:id])
+    end
+
+    def render_not_found_response 
+        render json: {error: "Movie not found"}, status: :not_found
+    end
 end
